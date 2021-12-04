c2123e6c97bef5a6ee144aedb989371b871ee682
ChrisMbwesse <94455277+ChrisMbwesse@users.noreply.github.com>
2021-12-03 23:52:27 -0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,27 @@
+public class Person
+{
+        private String name;
+        public Person(String initialName) {
+            name = initialName;
+        }
+
+        public Person() {
+        }
+        public void setName( String fullName) {
+
+            name = fullName;
+
+        }
+        public String getName() {
+
+            return name;
+        }
+
+        public String toString() {
+
+            System.out.println("Name: " + name);
+
+            return null;
+
+        }
+    }
